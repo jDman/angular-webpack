e470d85408e2e2bfc8e18e0316834f89003d3a06
Jay Davis <davis.jaym@gmail.com>
2016-04-08 16:17:03 +0100

    Configuring tests tbc

--- karma.conf.js
+++ karma.conf.js
@@ -18,14 +18,34 @@ module.exports = function karmaConfig (config) {
 
     files: [
       // Grab all files in the app folder that contain .spec.
-      'app/tests.webpack.js'
+      'app/tests.webpack.js',
+      'app/*.html',
+      'app/*.html.ext',
+      'app/**/*.html'
     ],
 
     preprocessors: {
       // Reference: http://webpack.github.io/docs/testing.html
       // Reference: https://github.com/webpack/karma-webpack
       // Convert files with webpack and load sourcemaps
-      'app/tests.webpack.js': ['webpack', 'sourcemap']
+      'app/tests.webpack.js': ['webpack', 'sourcemap'],
+      'app/**/*.html': ['ng-html2js']
+    },
+
+    ngHtml2JsPreprocessor: {
+      // strip this from the file path
+      stripPrefix: 'public/',
+      stripSuffix: '.ext',
+      // prepend this to the
+      prependPrefix: 'served/',
+
+      // - setting this option will create only a single module that contains templates
+      //   from all the files, so you can load them all with module('foo')
+      // - you may provide a function(htmlPath, originalPath) instead of a string
+      //   if you'd like to generate modules dynamically
+      //   htmlPath is a originalPath stripped and/or prepended
+      //   with all provided suffixes and prefixes
+      moduleName: 'foo'
     },
 
     browsers: [
